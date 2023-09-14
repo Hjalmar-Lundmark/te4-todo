891f6a17dc3d2dd157b01315dd32847dcf41b478
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-09-14 15:44:19 +0200

    Moved Footer inte own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Todolist from './components/Todolist';
+import Footer from './components/Footer';
 
 function App() {
 
     return (
-        <div className='container'>
-            <h1>Att göra lista</h1>
-            <Todolist />
-        </div>
+        <>
+            <div className='container'>
+                <h1>Att göra lista</h1>
+                <Todolist />
+            </div>
+            <Footer />
+        </>
     )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <div className="container">
+                <p>
+                    Sida skapad av <a href="https://github.com/Hjalmar-Lundmark">Hjalmar Lundmark</a> | <a href="https://github.com/Hjalmar-Lundmark/te4-react-navbar">Github repo</a>.
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
